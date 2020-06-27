4b4a10a6641c0b436645f2ed97fab25fde2fba07
jaeyoung.choi <lactrious@gmail.com>
2020-06-27 17:11:10 +0900

    Rearrange formats and modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import ReactQuill from 'react-quill';
-import './App.css';
-import 'react-quill/dist/quill.snow.css';
+import React from "react";
+import ReactQuill from "react-quill";
+import "./App.css";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
 
 function App() {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   return (
     <div className="App">
-        <ReactQuill theme="snow" value={value} onChange={setValue}/>
+      <ReactQuill
+        theme="snow"
+        formats={formats}
+        modules={modules}
+        value={value}
+        onChange={setValue}
+      />
     </div>
   );
 }
