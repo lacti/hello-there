5071b8552ad36396de87cd7cf79856e2db6fa2f6
jaeyoung.choi <lactrious@gmail.com>
2020-06-27 17:56:41 +0900

    Share via URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactQuill from "react-quill";
+import { compress, decompress } from "lzutf8";
 import "./App.css";
 import "react-quill/dist/quill.snow.css";
 import "react-quill/dist/quill.bubble.css";
@@ -36,9 +37,23 @@ const formats = [
 function App() {
   const [preview, setPreview] = React.useState(false);
   const [value, setValue] = React.useState("");
+
+  React.useEffect(() => {
+    const encoded = window.location.href.substring(window.location.href.indexOf("#") + 1);
+    if (encoded) {
+      setValue(decompress(encoded, { inputEncoding: "Base64" }));
+    }
+  }, []);
   return (
     <div className="App">
       <button onClick={() => setPreview(!preview)}>Toggle Preview</button>
+      <button
+        onClick={() =>
+          (window.location.href = "#" + compress(value, { outputEncoding: "Base64" }))
+        }
+      >
+        Share
+      </button>
       <ReactQuill
         theme={preview ? "bubble" : "snow"}
         formats={formats}
